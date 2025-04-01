3c202e4aa84a750cf07c47c00d0e56ff5b86e0f2
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-04-01 16:37:58 +0000

    merge my branch

--- carwebsite/carmodel_app/views.py
+++ carwebsite/carmodel_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render #type: ignore
 
 # Create your views here.
+def index(request):
+    return render(request, 'carmodel/index.html') #type: ignore
