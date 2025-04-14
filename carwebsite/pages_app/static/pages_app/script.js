d8e2e42d63a5384f767a57eb679180e3d5834468
FOLISOLOMON <foliremedy50@gmail.com>
2025-04-14 16:42:16 +0400

    I have updatded the html file

--- carwebsite/pages_app/static/pages_app/script.js
+++ carwebsite/pages_app/static/pages_app/script.js
@@ -12,3 +12,16 @@ close.addEventListener('click', () => {
     nav_links_box.classList.remove("show")
     overlay.style.display = "none";
 })
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: "smooth"
+            });
+        }
+    });
+});
